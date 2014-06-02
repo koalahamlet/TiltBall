9634c2d0321c66700cdfcd318c293d921261a26a
koalahamlet <koalahamlet@gmail.com>
2014-06-02 14:32:33 -0700

    Add notes for making it an actual color tilt.

--- app/src/main/java/droid/pkg/TiltBallActivity.java
+++ app/src/main/java/droid/pkg/TiltBallActivity.java
@@ -28,6 +28,7 @@ public class TiltBallActivity extends Activity {
 	TimerTask mTsk = null;
 	int mScrWidth, mScrHeight;
     android.graphics.PointF mBallPos, mBallSpd;
+    FrameLayout mainView = null;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -37,7 +38,7 @@ public class TiltBallActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         //create pointer to main screen
-        final FrameLayout mainView = (android.widget.FrameLayout) findViewById(R.id.main_view);
+        mainView = (android.widget.FrameLayout) findViewById(R.id.main_view);
 
         //get screen dimensions
         Display display = getWindowManager().getDefaultDisplay();  
@@ -55,6 +56,7 @@ public class TiltBallActivity extends Activity {
         mBallSpd.x = 0;
         mBallSpd.y = 0;
 
+
         
         //create initial ball
         mBallView = new BallView(this,mBallPos.x,mBallPos.y,5, 0xFF00FF00);
@@ -62,23 +64,25 @@ public class TiltBallActivity extends Activity {
         mainView.addView(mBallView); //add ball to main screen
         mBallView.invalidate(); //call onDraw in BallView
 
-        		
         //listener for accelerometer, use anonymous class for simplicity
-        ((SensorManager)getSystemService(Context.SENSOR_SERVICE)).registerListener(
-    		new SensorEventListener() {    
-    			@Override  
-    			public void onSensorChanged(SensorEvent event) {  
-    			    //set ball speed based on phone tilt (ignore Z axis)
-    				mBallSpd.x = -event.values[0];
-    				mBallSpd.y = event.values[1];
+                ((SensorManager) getSystemService(Context.SENSOR_SERVICE)).registerListener(
+                        new SensorEventListener() {
+                            @Override
+                            public void onSensorChanged(SensorEvent event) {
+                                //set ball speed based on phone tilt (ignore Z axis)
+                                mBallSpd.x = -event.values[0];
+                                mBallSpd.y = event.values[1];
 
-    				//timer event will redraw ball
-    			}
-        		@Override  
-        		public void onAccuracyChanged(Sensor sensor, int accuracy) {} //ignore this event
-        	},
-        	((SensorManager)getSystemService(Context.SENSOR_SERVICE))
-        	.getSensorList(Sensor.TYPE_ACCELEROMETER).get(0), SensorManager.SENSOR_DELAY_NORMAL);
+                                //timer event will redraw ball
+                            }
+
+                            @Override
+                            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+                            } //ignore this event
+                        },
+                        ((SensorManager) getSystemService(Context.SENSOR_SERVICE))
+                                .getSensorList(Sensor.TYPE_ACCELEROMETER).get(0), SensorManager.SENSOR_DELAY_NORMAL
+                );
         		
         //listener for touch event 
         mainView.setOnTouchListener(new android.view.View.OnTouchListener() {
@@ -142,6 +146,29 @@ public class TiltBallActivity extends Activity {
 				//update ball class instance
 				mBallView.mX = mBallPos.x;
 				mBallView.mY = mBallPos.y;
+
+
+
+
+                // when you come back to looking at updating background color
+                // you'll do it here.
+
+                // Color
+                //        *     hsv[0] is Hue [0 .. 360)
+                //        *     hsv[1] is Saturation [0...1]
+                //        *     hsv[2] is Value [0...1]
+                // use this for reference if ^^ doesn't make sense http://hslpicker.com/#ff0a0a
+                //  mainView.setBackgroundColor(0xFF00FF00);
+
+//                float[] hsv = new float[3];
+//                hsv[0] = 50f; // color
+//                hsv[1] = 1f; // how close to grey, should probably be 1.0
+//                hsv[2] = 0.5f; // how close to white(1) or black(0),  could perhaps
+//                // calculate it based on proximity to ends. cen
+//                int color = Color.HSVToColor(1, hsv);
+//                mainView.setBackgroundColor(color);
+
+
 
 				//redraw ball. Must run in background thread to prevent thread lock.
 				RedrawHandler.post(new Runnable() {
